8262223ba88b4b50e8163322e05b66807ffc385f
Titus Denis <deetitos@gmail.com>
2024-01-20 02:37:41 +0100

    0-bubble_sort.c Added

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,22 +9,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	bool swapped;
-	size_t i = 0, j;
+	size_t i, j;
+	int temp;
 
-	do {
-	swapped = false;
-	for (j = 0; j < size - 1 - i; j++)
+	for (i = 0; i < size; i++)
 	{
-		if (array[i] > array[i + 1])
+		for (j = 0; j < (size - 1); j++)
 		{
-			temp = array[i];
-			array[i] = array[i + 1];
-			array[i + 1] = temp;
-			print_array(array, size);
-			swapped = true;
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
 		}
 	}
-	i++;
-	} while (swapped);
 }
